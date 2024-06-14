f9dcfdd30c2a0b795298f42e09a140519539ec15
Bonnie <biankabonete@gmail.com>
2024-06-14 15:37:35 -0300

    Adicionadas classes de conta Corrente e Poupança

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -3,11 +3,14 @@ package conta;
 import java.util.Scanner;
 import conta.util.Cores;
 import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 
 public class Menu {
 
 	public static void main(String[] args) {
 		
+		// Teste da classe Conta
 		Conta c1 = new Conta(0001, 123, 1, "Renata", 10000.0f);
 		c1.visualizar();
 		c1.sacar(12000.0f);
@@ -15,6 +18,21 @@ public class Menu {
 		c1.depositar(5000.0f);
 		c1.visualizar();
 		
+		// Teste da classe ContaCorrente
+		ContaCorrente cc1 = new ContaCorrente(0002,123,1,"Mariana",15000.0f,1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+		
+		// Teste de classe ContaPoupanca
+		ContaPoupanca cp1 = new ContaPoupanca(0003,123,2,"Victor",100000.0f, 15);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
 		
 		
 		
